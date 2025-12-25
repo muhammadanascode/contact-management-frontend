ee8b2f3725d07e88b31c86254de2cd80a1327d24
muhammadanascode <anassohal34343@gmail.com>
2025-12-25 22:30:28 +0500

    refactor: Refactoring the code taking use of components

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import InputField from "./InputField";
+import Button from "./Button";
 
 function AuthForm({ type }) {
     const [name, setName] = useState("");
@@ -41,52 +43,37 @@ function AuthForm({ type }) {
 
                 {type !== "Login" ? <div className="flex flex-col">
                     <label className="mb-1 text-gray-700 font-medium">Fullname:</label>
-                    <input
-                        type="text"
+                    <InputField
+                        type={"text"}
                         value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                        className="w-full px-3 py-2 border border-gray-300 rounded-md
-             focus:outline-none focus:ring-2 focus:ring-blue-500
-             focus:border-blue-500 transition"
+                        setValue={setName}
                     />
                 </div> : null}
 
                 <div className="flex flex-col">
                     <label className="mb-1 text-gray-700 font-medium">Email:</label>
-                    <input
-                        type="email"
+                    <InputField
+                        type={"email"}
                         value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                        className="w-full px-3 py-2 border border-gray-300 rounded-md
-             focus:outline-none focus:ring-2 focus:ring-blue-500
-             focus:border-blue-500 transition"
+                        setValue={setEmail}
                     />
                 </div>
 
                 <div className="flex flex-col">
                     <label className="mb-1 text-gray-700 font-medium">Password:</label>
-                    <input
-                        type="password"
+                    <InputField
+                        type={"password"}
                         value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                        className="w-full px-3 py-2 border border-gray-300 rounded-md
-             focus:outline-none focus:ring-2 focus:ring-blue-500
-             focus:border-blue-500 transition"
+                        setValue={setPassword}
                     />
                 </div>
 
-                <button
-                    type="submit"
-                    className="w-full bg-blue-600 text-white py-2.5 rounded-md
-             font-medium hover:bg-blue-700
-             focus:outline-none focus:ring-2 focus:ring-blue-500
-             transition"
-                >
-                    {type === "Login" ? "Login" : "Signup"}
-                </button>
+
+                <Button
+                    type={"submit"}
+                    text={type === "Login" ? "Login" : "Signup"}
+                />
+
                 {type === "Login" ? (
                     <p className="text-center text-sm text-gray-600">
                         Not having an account?{" "}
