95caff040545a39ae2082201795fcd8c10b1fded
muhammadanascode <anassohal34343@gmail.com>
2025-12-28 01:13:03 +0500

    refactor: remove div wrapper and use custom componentfor input field

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
+import InputField from "./InputField";
+
 const SearchBar = () => {
+    const[value , setValue] = useState("");
+
     return (
-        <div className="flex flex-col sm:flex-row gap-4 mb-6">
-            <input
-                type="text"
-                placeholder="Search contacts by name or phone..."
-                className="flex-1 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-        </div>
+        <InputField
+            type="text"
+            placeholder="Search contacts by name or phone..."
+            value={value}
+            setValue={setValue}
+        />
     );
 };
 
