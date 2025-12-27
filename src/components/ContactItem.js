ad1b40cf3811bf528d9587716e36bd9b0257d399
muhammadanascode <anassohal34343@gmail.com>
2025-12-27 15:02:14 +0500

    feat: Added icons istead of text

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,11 +1,17 @@
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
+
 const ContactItem = ({ contact }) => {
   return (
     <tr className="border-b last:border-none">
       <td className="py-3 px-4">{contact.name}</td>
       <td className="py-3 px-4">{contact.phone}</td>
       <td className="py-3 px-4 text-right space-x-2">
-        <button className="text-blue-600 text-sm">Edit</button>
-        <button className="text-red-600 text-sm">Delete</button>
+        <button className="text-gray-600 text-sm">
+            <PencilIcon className="w-5 h-5" />
+        </button>
+        <button className="text-gray-600 text-sm">
+            <TrashIcon className="w-5 h-5" /> {/* Delete */}
+        </button>
       </td>
     </tr>
   );
