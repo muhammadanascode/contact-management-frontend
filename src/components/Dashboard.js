2674823bb099063017bacaa2cf5e9ad0c6bfb5ac
muhammadanascode <anassohal34343@gmail.com>
2025-12-28 02:44:39 +0500

    feat: Added modal for creating new contact

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,8 +2,27 @@ import ContactList from "./ContactList";
 import Navbar from "./Navbar";
 import SearchBar from "./SearchBar";
 import Button from "./Button";
+import Modal from "./Modal";
+import InputField from "./InputField";
+import { useState } from "react";
 
 const Dashboard = () => {
+
+    //State to control modal visibility and form inputs
+    const [isOpen, setIsOpen] = useState(false);
+    const [name, setName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+
+    // Handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Name: " + name, "phoneNumber: " + phoneNumber);
+        setName("");
+        setPhoneNumber("");
+        setIsOpen(false);
+    }
+
+
     return (
         <div className="min-h-screen bg-gray-100">
             <Navbar />
@@ -18,10 +37,35 @@ const Dashboard = () => {
                     <Button
                         text="+ Add Contact"
                         className="w-full sm:w-auto px-6 py-2 shrink-0"
+                        onClick={() => setIsOpen(true)}
                     />
                 </div>
 
+                {/* Modal for creating new contact */}
+                <Modal
+                    isOpen={isOpen}
+                    onClose={() => setIsOpen(false)}
+                    title={"Create contact"}
+                    onSubmit={handleSubmit}
+                >
+                    <InputField
+                        type="text"
+                        value={name}
+                        setValue={setName}
+                        placeholder="Name"
 
+                    />
+
+                    <InputField
+                        type="text"
+                        value={phoneNumber}
+                        setValue={setPhoneNumber}
+                        placeholder="Phone Number"
+
+                    />
+                </Modal>
+
+                {/* Contact List Table */}
                 <ContactList />
             </main>
         </div>
