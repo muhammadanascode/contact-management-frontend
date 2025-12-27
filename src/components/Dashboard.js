18d084c2ab3b4cfaf4a31af2572a453a1a3af281
muhammadanascode <anassohal34343@gmail.com>
2025-12-28 01:14:43 +0500

    refactor: reformatted the code dashboard code

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,25 +4,25 @@ import SearchBar from "./SearchBar";
 import Button from "./Button";
 
 const Dashboard = () => {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <Navbar />
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <Navbar />
 
-      <main className="max-w-6xl mx-auto p-6">
-        {/* Search + Action Row */}
-        <div className="flex flex-col sm:flex-row gap-4 mb-6">
-          <SearchBar />
+            <main className="max-w-6xl mx-auto p-6">
+                {/* Search + Action Row */}
+                <div className="flex flex-col sm:flex-row gap-4 mb-6">
+                    <SearchBar />
 
-          <Button
-            text="+ Add Contact"
-            className="w-full sm:w-auto px-6"
-          />
+                    <Button
+                        text="+ Add Contact"
+                        className="w-full sm:w-auto px-6"
+                    />
+                </div>
+
+                <ContactList />
+            </main>
         </div>
-
-        <ContactList />
-      </main>
-    </div>
-  );
+    );
 };
 
 export default Dashboard;
