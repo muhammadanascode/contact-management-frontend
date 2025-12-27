c550c8f38c291ba18557f47ae7ba6c87c6d63343
muhammadanascode <anassohal34343@gmail.com>
2025-12-27 14:51:52 +0500

    style: changed bg color of buttons and text

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,27 +1,27 @@
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
     return (
-        <div className="flex justify-center gap-2 mt-4">
+        <div className="flex justify-center gap-2 mt-4 py-2">
             <button
                 disabled={currentPage === 1}
                 onClick={() => onPageChange(currentPage - 1)}
-                className="px-3 py-1 border rounded disabled:opacity-50"
+                className="px-3 py-1 font-bold border bg-purple-700 text-white  rounded disabled:opacity-50"
             >
-                Previous
+                {"<"}
             </button>
 
             <button
-                className="px-3 py-1 border rounded bg-blue-500 text-white"
+                className="px-3 py-1 border rounded text-black"
             >
-                {currentPage}
+                {currentPage} of {totalPages}
             </button>
 
 
             <button
                 disabled={currentPage === totalPages}
                 onClick={() => onPageChange(currentPage + 1)}
-                className="px-3 py-1 border rounded disabled:opacity-50"
+                className="px-3 py-1 font-bold border bg-purple-700 text-white  rounded disabled:opacity-50"
             >
-                Next
+                {">"}
             </button>
         </div>
     );
