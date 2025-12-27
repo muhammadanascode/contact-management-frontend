e40d03b80d9f2a9982d8080f7bb3c2764d68421d
muhammadanascode <anassohal34343@gmail.com>
2025-12-28 01:12:05 +0500

    feat: Added placeholder

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,6 +1,4 @@
-import React from "react";
-
-function InputField({type , value , setValue}) {
+function InputField({type , value , setValue , placeholder}) {
     return (
         <>
             <input
@@ -10,7 +8,8 @@ function InputField({type , value , setValue}) {
                 required
                 className="w-full px-3 py-2 border border-gray-300 rounded-md
              focus:outline-none focus:ring-2 focus:ring-blue-500
-             focus:border-blue-500 transition"
+             focus:border-blue-500 transition" 
+             placeholder={placeholder}
             />
         </>
     )
