99b9f98fb5fff6240240b8b046e9fb6dcfa821ea
muhammadanascode <anassohal34343@gmail.com>
2025-12-27 14:44:22 +0500

    refactor: reformat code

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,30 +1,30 @@
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  return (
-    <div className="flex justify-center gap-2 mt-4">
-      <button
-        disabled={currentPage === 1}
-        onClick={() => onPageChange(currentPage - 1)}
-        className="px-3 py-1 border rounded disabled:opacity-50"
-      >
-        Previous
-      </button>
+    return (
+        <div className="flex justify-center gap-2 mt-4">
+            <button
+                disabled={currentPage === 1}
+                onClick={() => onPageChange(currentPage - 1)}
+                className="px-3 py-1 border rounded disabled:opacity-50"
+            >
+                Previous
+            </button>
 
-      <button
-  className="px-3 py-1 border rounded bg-blue-500 text-white"
->
-  {currentPage}
-</button>
+            <button
+                className="px-3 py-1 border rounded bg-blue-500 text-white"
+            >
+                {currentPage}
+            </button>
 
 
-      <button
-        disabled={currentPage === totalPages}
-        onClick={() => onPageChange(currentPage + 1)}
-        className="px-3 py-1 border rounded disabled:opacity-50"
-      >
-        Next
-      </button>
-    </div>
-  );
+            <button
+                disabled={currentPage === totalPages}
+                onClick={() => onPageChange(currentPage + 1)}
+                className="px-3 py-1 border rounded disabled:opacity-50"
+            >
+                Next
+            </button>
+        </div>
+    );
 };
 
 export default Pagination;
