cba583371af226984c0ad35d56c69cf9ca4be7c5
muhammadanascode <anassohal34343@gmail.com>
2025-12-28 02:45:32 +0500

    feat: Added callback to trigger modal

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,12 +1,12 @@
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
 
-const ContactItem = ({ contact }) => {
+const ContactItem = ({ contact, onEdit }) => {
     return (
         <tr className="border-b last:border-none">
             <td className="py-3 px-4">{contact.name}</td>
             <td className="py-3 px-4">{contact.phone}</td>
             <td className="py-3 px-4 text-right space-x-2">
-                <button className="text-gray-600 text-sm">
+                <button className="text-gray-600 text-sm" onClick={() => onEdit(contact)}>
                     <PencilIcon className="w-5 h-5" />
                 </button>
                 <button className="text-gray-600 text-sm">
