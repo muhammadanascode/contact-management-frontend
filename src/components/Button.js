2de5411e8f701dee6dc29d7ab217183dd0b9c350
muhammadanascode <anassohal34343@gmail.com>
2025-12-27 14:14:07 +0500

    fix: Added className so that button can be used as a shared component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,12 @@
-import React from "react";
-
-function Button({ type, text }) {
+function Button({ type="button", text , className }) {
     return (
         <>
             <button
                 type={type}
-                className="w-full bg-purple-600 text-white py-2.5 rounded-md
+                className={` bg-purple-600 text-white py-2.5 rounded-md
              font-medium hover:bg-purple-800
              focus:outline-none focus:ring-2 focus:ring-blue-500
-             transition"
+             transition` + className}
             >{text}</button>
         </>
     )
