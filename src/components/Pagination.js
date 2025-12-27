f20c0378e482d06d6fccfb382450c8503d746af7
muhammadanascode <anassohal34343@gmail.com>
2025-12-27 14:42:25 +0500

    fix: Displaying only current page button instead of all and justified div in center

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,6 @@
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   return (
-    <div className="flex justify-end gap-2 mt-4">
+    <div className="flex justify-center gap-2 mt-4">
       <button
         disabled={currentPage === 1}
         onClick={() => onPageChange(currentPage - 1)}
@@ -9,17 +9,12 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
         Previous
       </button>
 
-      {[...Array(totalPages)].map((_, i) => (
-        <button
-          key={i}
-          onClick={() => onPageChange(i + 1)}
-          className={`px-3 py-1 border rounded ${
-            currentPage === i + 1 ? "bg-blue-500 text-white" : ""
-          }`}
-        >
-          {i + 1}
-        </button>
-      ))}
+      <button
+  className="px-3 py-1 border rounded bg-blue-500 text-white"
+>
+  {currentPage}
+</button>
+
 
       <button
         disabled={currentPage === totalPages}
