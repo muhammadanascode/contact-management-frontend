039faf51b789421a3d3cfc87f3b61b1b3baafb35
muhammadanascode <anassohal34343@gmail.com>
2025-12-27 14:15:04 +0500

    feat: Added a search bar in dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,14 @@
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 
 const Dashboard = () => {
     return (
         <div className="min-h-screen bg-gray-100">
             <Navbar />
+
+            <main className="max-w-6xl mx-auto p-6">
+                <SearchBar />
+            </main>
         </div>
     );
 };
