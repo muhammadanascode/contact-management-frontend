dd6ecffb8ce840319f7ab422c362a6a39c8f2125
muhammadanascode <anassohal34343@gmail.com>
2025-12-27 13:21:00 +0500

    feat: Added a navabr on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,11 @@
+import Navbar from "./Navbar";
+
 const Dashboard = () => {
-  return (
-    <div className="min-h-screen bg-gray-100">
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <Navbar />
+        </div>
+    );
 };
 
 export default Dashboard;
