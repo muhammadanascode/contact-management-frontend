d4e8c4091dbfaed2abe1b7324d3b318b4056aa80
muhammadanascode <anassohal34343@gmail.com>
2025-12-27 14:30:48 +0500

    feat: Created a pagination component so that  the data can be shown as paginated list

---
+++ src/components/Pagination.js
@@ -0,0 +1,35 @@
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  return (
+    <div className="flex justify-end gap-2 mt-4">
+      <button
+        disabled={currentPage === 1}
+        onClick={() => onPageChange(currentPage - 1)}
+        className="px-3 py-1 border rounded disabled:opacity-50"
+      >
+        Previous
+      </button>
+
+      {[...Array(totalPages)].map((_, i) => (
+        <button
+          key={i}
+          onClick={() => onPageChange(i + 1)}
+          className={`px-3 py-1 border rounded ${
+            currentPage === i + 1 ? "bg-blue-500 text-white" : ""
+          }`}
+        >
+          {i + 1}
+        </button>
+      ))}
+
+      <button
+        disabled={currentPage === totalPages}
+        onClick={() => onPageChange(currentPage + 1)}
+        className="px-3 py-1 border rounded disabled:opacity-50"
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
