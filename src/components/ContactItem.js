3b8bc1bdb9c04ca0fae9672b059a66e7f4c44900
muhammadanascode <anassohal34343@gmail.com>
2025-12-27 15:02:30 +0500

    refactor: formatted code

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,20 +1,20 @@
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
 
 const ContactItem = ({ contact }) => {
-  return (
-    <tr className="border-b last:border-none">
-      <td className="py-3 px-4">{contact.name}</td>
-      <td className="py-3 px-4">{contact.phone}</td>
-      <td className="py-3 px-4 text-right space-x-2">
-        <button className="text-gray-600 text-sm">
-            <PencilIcon className="w-5 h-5" />
-        </button>
-        <button className="text-gray-600 text-sm">
-            <TrashIcon className="w-5 h-5" /> {/* Delete */}
-        </button>
-      </td>
-    </tr>
-  );
+    return (
+        <tr className="border-b last:border-none">
+            <td className="py-3 px-4">{contact.name}</td>
+            <td className="py-3 px-4">{contact.phone}</td>
+            <td className="py-3 px-4 text-right space-x-2">
+                <button className="text-gray-600 text-sm">
+                    <PencilIcon className="w-5 h-5" />
+                </button>
+                <button className="text-gray-600 text-sm">
+                    <TrashIcon className="w-5 h-5" /> {/* Delete */}
+                </button>
+            </td>
+        </tr>
+    );
 };
 
 export default ContactItem;
