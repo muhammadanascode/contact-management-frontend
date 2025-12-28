43dbc668f5acd0cb5293559c72425e67350af894
muhammadanascode <anassohal34343@gmail.com>
2025-12-28 21:03:23 +0500

    style: style the button to be displayed on the left instead of center

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,7 +29,7 @@ const Dashboard = () => {
 
             <main className="max-w-6xl mx-auto p-6">
                 {/* Search + Action Row */}
-                <div className="flex flex-col sm:flex-row gap-4 mb-3 items-center">
+                <div className="flex flex-col sm:flex-row gap-4 mb-3 items-start sm:items-center">
                     <div className="flex-1 w-full">
                         <SearchBar />
                     </div>
