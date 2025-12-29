2ac6367a0fe8250b44fb4da81df363e7f9d041c7
muhammadanascode <anassohal34343@gmail.com>
2025-12-29 14:46:56 +0500

    feat: added a function to perform to open confirmation modal for delete operation

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,6 +1,6 @@
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
 
-const ContactItem = ({ contact, onEdit }) => {
+const ContactItem = ({ contact, onEdit , onDelete }) => {
     return (
         <tr className="border-b last:border-none">
             <td className="py-3 px-4">{contact.name}</td>
@@ -9,7 +9,7 @@ const ContactItem = ({ contact, onEdit }) => {
                 <button className="text-gray-600 text-sm" onClick={() => onEdit(contact)}>
                     <PencilIcon className="w-5 h-5" />
                 </button>
-                <button className="text-gray-600 text-sm">
+                <button className="text-gray-600 text-sm" onClick={()=> onDelete(contact)}>
                     <TrashIcon className="w-5 h-5" /> {/* Delete */}
                 </button>
             </td>
