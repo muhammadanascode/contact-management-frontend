08dc38dedc5ca6d6b8c287a81da5be0f7855c52e
muhammadanascode <anassohal34343@gmail.com>
2025-12-29 18:39:28 +0500

    feat: Added a modal to update password

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import Button from "../components/Button";
+import Modal from "./Modal";
+import InputField from "./InputField";
 
 function ProfilePage() {
     // Temporary static data (will come from backend later)
@@ -7,10 +10,61 @@ function ProfilePage() {
         email: "john.doe@example.com",
     };
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmNewPassword, setConfirmNewPassword] = useState("");
+
+    const handleUpdatePassword = (e) => {
+        e.preventDefault();
+        console.log("Current Password: " + currentPassword);
+        console.log("New Password: " + newPassword);
+        console.log("Confirm New Password: " + confirmNewPassword);
+        setCurrentPassword("");
+        setNewPassword("");
+        setConfirmNewPassword("");
+    }
+
     return (
         <>
             {/* Page container */}
             <div className="max-w-4xl mx-auto p-6">
+
+                <Modal
+                    isOpen={isModalOpen}
+                    onClose={() => {
+                        setIsModalOpen(false);
+                        setCurrentPassword("");
+                        setNewPassword("");
+                        setConfirmNewPassword("");
+                    }}
+
+                    title={"Update Password"}
+                    onSubmit={handleUpdatePassword}
+                >
+                    <InputField
+                        type="password"
+                        placeholder="Current Password"
+                        value={currentPassword}
+                        setValue={setCurrentPassword}
+                    />
+
+                    <InputField
+                        type="password"
+                        placeholder="New Password"
+                        value={newPassword}
+                        setValue={setNewPassword}
+                    />
+
+                    <InputField
+                        type="password"
+                        placeholder="Confirm New Password"
+                        value={confirmNewPassword}
+                        setValue={setConfirmNewPassword}
+                    />
+
+                </Modal>
 
                 {/* Page Header */}
                 <div className="mb-6 mt-4">
@@ -68,6 +122,7 @@ function ProfilePage() {
                         <Button
                             text="Update Password"
                             className=" px-4"
+                            onClick={() => setIsModalOpen(true)}
                         />
                     </div>
                 </div>
