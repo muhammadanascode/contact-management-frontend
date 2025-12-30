ba2954c87e5eca44a968a67b55f744f697c97bda
muhammadanascode <anassohal34343@gmail.com>
2025-12-30 20:26:56 +0500

    chore: disbaling the spinner of nprogress bar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'nprogress/nprogress.css';
+import NProgress from 'nprogress';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -15,3 +17,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// nprogress configuration: disable the spinner
+NProgress.configure({ showSpinner: false });
