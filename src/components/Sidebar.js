81e24dbc5167750d2097c272ce2fdb0f6136f706
muhammadanascode <anassohal34343@gmail.com>
2025-12-31 14:34:42 +0500

    feat: Add logo and welcome message to Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import logo from '../assests/images/social-media.png';
 
 const Sidebar = ({ isOpen, onClose }) => {
     const handleNavClick = () => {
@@ -17,13 +18,26 @@ const Sidebar = ({ isOpen, onClose }) => {
 
             {/* Sidebar */}
             {isOpen && (
-                <div className="sm:hidden fixed left-0 top-0 h-full w-64 bg-white shadow-lg z-50 pt-6 px-4">
+                <div className="sm:hidden fixed left-0 top-0 h-full w-64 bg-white shadow-lg z-50 pt-2 px-4">
                     <button
-                        className="mb-6 text-gray-800 text-2xl font-bold"
+                        className="mb-6 text-gray-800 text-xl font-bold"
                         onClick={onClose}
                     >
-                        ✕
+                        &times;
                     </button>
+
+                    {/* Logo / App Name */}
+                    <div className="flex items-center gap-3 mb-8">
+                        <img
+                            src={logo}
+                            className="h-7 w-7 sm:h-10 sm:w-10 object-contain"
+                            alt="Logo"
+                        />
+                        <h1 className="text-base text-lg font-bold text-gray-800">
+                            ConnectHub CMS
+                        </h1>
+                    </div>
+
                     <nav className="flex flex-col gap-4">
                         <NavLink
                             to="/"
@@ -51,6 +65,13 @@ const Sidebar = ({ isOpen, onClose }) => {
                             Profile
                         </NavLink>
                     </nav>
+
+                    {/* Profile Section */}
+                    <div className="mt-10 border-t pt-4">
+                        <h2 className="text-md font-semibold mb-4 text-gray-800">
+                            Welcome, Alex Smith
+                        </h2>
+                    </div>
                 </div>
             )}
         </>
