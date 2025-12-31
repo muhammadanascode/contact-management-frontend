4d05ea62b7821f05b7400c2501536a56f40e2f56
muhammadanascode <anassohal34343@gmail.com>
2025-12-31 12:29:57 +0500

    feat: integrate global toast notifications at app root

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppContent from "./router/AppContent";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-center" />
       <AppContent />
     </BrowserRouter>
   );
 }
 
-export default App ;
+export default App;
