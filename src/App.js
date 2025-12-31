62b8572c094a08d75d8f9a6f6cbf77e833a30a07
muhammadanascode <anassohal34343@gmail.com>
2025-12-31 12:39:45 +0500

    chore: Added route loader component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import AppContent from "./router/AppContent";
 import { Toaster } from "react-hot-toast";
+import RouteLoader from "./components/RouteLoader";
 
 function App() {
   return (
     <BrowserRouter>
+      <RouteLoader />
       <Toaster position="top-center" />
       <AppContent />
     </BrowserRouter>
