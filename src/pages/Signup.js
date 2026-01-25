82ec2b660e1d6cae5e281a104cc9d52761c682ea
muhammadanascode <anassohal34343@gmail.com>
2026-01-25 21:51:50 +0500

    feat: created handler and passing to form to get data

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,15 @@
-import React from "react";
 import AuthForm from "../components/AuthForm";
+import { signup } from "../services/AuthService";
 
 function Signup() {
+
+  // Handler to pass to AuthForm for signup
+  const handleSignup = (firstName, lastName, email, password) => {
+    signup(firstName, lastName, email, password);
+  }
+
   return (
-      <AuthForm type="Signup" />
+      <AuthForm type="Signup" handleSignup={handleSignup} />
   );
 }
 
