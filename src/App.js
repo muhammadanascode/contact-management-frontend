c93997dae0d9a448c0eb17f7f68ffc0dc6a63d62
muhammadanascode <anassohal34343@gmail.com>
2026-01-28 10:18:14 +0500

    feat: wrapping App with Authprovider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import AppContent from "./router/AppContent";
 import { Toaster } from "react-hot-toast";
 import RouteLoader from "./components/RouteLoader";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <RouteLoader />
-      <Toaster position="top-center" />
-      <AppContent />
+      <AuthProvider>
+        <RouteLoader />
+        <Toaster position="top-center" />
+        <AppContent />
+      </AuthProvider>
     </BrowserRouter>
   );
 }
