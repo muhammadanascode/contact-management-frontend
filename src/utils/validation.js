3144bb9dedab150249ddff0b831d4b07599e0bf2
muhammadanascode <anassohal34343@gmail.com>
2026-01-29 23:37:28 +0500

    feat: added a method to check token expiry

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,3 +1,5 @@
+import { jwtDecode } from "jwt-decode";
+
 export const validateName = (name) => {
   return name?.length >= 3 && name?.length <= 50;
 };
@@ -10,4 +12,14 @@ export const validateEmail = (email) => {
   if (!email) return false;
   const emailRegex = /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/;
   return emailRegex.test(email);
+};
+
+// validation: check token expiry
+export const isTokenValid = (token) => {
+  try {
+    const { exp } = jwtDecode(token);
+    return exp * 1000 > Date.now();
+  } catch {
+    return false;
+  }
 };
