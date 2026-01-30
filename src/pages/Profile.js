4bb312368295cb7ffc5f9323dee24daaf91ea8f6
muhammadanascode <anassohal34343@gmail.com>
2026-01-30 09:41:09 +0500

    feat:added use effect to fetch user's profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,34 @@
+import { useState, useEffect } from "react";
 import ProfilePage from "../components/ProfilePage";
+import { getProfile } from "../services/ProfileService";
+import { useAuth } from "../context/AuthContext";
 
 const Profile = () => {
-    return (
-        <ProfilePage />
-    )
-}
+    const [profile, setProfile] = useState(null); // start as null
+    const [loading, setLoading] = useState(true);
+    const { token } = useAuth();
+
+    useEffect(() => {
+        if (!token) {
+            setLoading(false);
+            return;
+        }
+        const fetchProfile = async () => {
+            setLoading(true);
+            const profileData = await getProfile(token);
+            if (profileData) {
+                setProfile(profileData);
+            }
+            setLoading(false);
+        };
+
+        fetchProfile();
+    }, []);
+
+    if (loading) return <p>Loading profile...</p>;
+    if (!profile) return <p>Failed to load profile</p>;
+
+    return <ProfilePage profile={profile} />;
+};
 
 export default Profile;
